5174bb248bcd0f14beb9aa8368bd3eef79961dde
Aleksandar Antonov <ala021@bucknell.edu>
2017-10-25 13:01:40 -0400

    Completed Table, Added Legend to Plot, cleaned out unecessary test and reference directories

--- Shiny Apps/TimeVarVis/app.R
+++ Shiny Apps/TimeVarVis/app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(tidyverse)
+library(devtools)
 library(ggplot2)
 
 #EmotiVis, Bucknell Senior Design
@@ -33,18 +35,26 @@ ui <- fluidPage(
                                Tab = "\t"),
                    selected = ","), 
       
-      
+
       
       # Horizontal line ----
-      tags$hr()
+      tags$hr(),
       
-      
+      # Input: Select number of rows to display ----
+      radioButtons("disp", "Display",
+                   choices = c(All = "all",
+                               head = "head"),
+                   #Defaut to displaying all data in table
+                   selected = "all") 
       ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      plotOutput("timeSeries")
-      
+      tabsetPanel(
+        tabPanel("Plot",  plotOutput("timeSeries")), 
+        tabPanel("Summary", plotOutput("summary")), 
+        tabPanel("Table", tableOutput("table"))
+      )
       
     )
     
@@ -73,17 +83,69 @@ server <- function(input, output) {
     emotiplot = plotdata()
     emoti_sub = emotiplot[,c(2:10)]
     ggplot(emotiplot, aes(x = Time, group = 1)) +
-      geom_line(aes(y = Joy), colour="blue") +
-      geom_line(aes(y = Sadness), colour = "red") +
-      geom_line(aes(y = Disgust), colour = "green") +
-      geom_line(aes(y = Contempt), colour="orange") +
-      geom_line(aes(y = Anger), colour = "yellow") +
-      geom_line(aes(y = Fear), colour = "pink") +
-      geom_line(aes(y = Surprise), colour = "grey") +
-      geom_line(aes(y = Valence), colour="coral") +
-      geom_line(aes(y = Engagement), colour = "skyblue") +
+      geom_line(aes(y = Joy, colour="Joy")) +
+      geom_line(aes(y = Sadness, colour = "Sadness")) +
+      geom_line(aes(y = Disgust, colour = "Disgust")) +
+      geom_line(aes(y = Contempt, colour="Contempt")) +
+      geom_line(aes(y = Anger, colour = "Anger")) +
+      geom_line(aes(y = Fear, colour = "Fear")) +
+      geom_line(aes(y = Surprise, colour = "Surprise")) +
+      geom_line(aes(y = Valence, colour="Valence")) +
+      geom_line(aes(y = Engagement, colour = "Engagement")) +
       ylab(label="Emotional Measure") + xlab("Time") +
-      labs(title = "Time Varied Emotional Response")
+      labs(title = "Time Varied Emotional Response")+
+      scale_fill_manual(name = "Emotions", values = c("blue", "red", "orange"))
+  })
+  
+  #Visualize Table with Option to display desired number of rows of data
+  output$table <- renderTable({
+    if(input$disp == "head") {
+      return(head(plotdata()))
+    }
+    else {
+      return(plotdata())
+    }
+  })
+
+  
+  #Visualize Barchart taking means of every emotion column in the CSV
+  output$summary <- renderPlot({
+    
+    emo = plotdata()
+    #emoMean <- colMeans(plotdata())
+    emoMean = NULL
+    emoMean$joy <- mean(emo$Joy)
+    emoMean$sadness <- mean(emo$Sadness)
+    emoMean$disgust <- mean(emo$Disgust)
+    emoMean$contempt <- mean(emo$Contempt)
+    emoMean$anger <- mean(emo$Anger)
+    emoMean$fear <- mean(emo$Fear)
+    emoMean$surprise <- mean(emo$Surprise)
+    emoMean$valence <- mean(emo$Valence)
+    emoMean$engagement <- mean(emo$Engagement)
+    ggplot(gather(emo, cols, value), aes(x = value)) + 
+      geom_histogram(binwidth = 20) + facet_grid(.~cols)
+    # emo2 <- emo %>%
+    #   gather(
+    #     `joy`,
+    #     `sadness`,
+    #     `disgust`,
+    #     `contempt`,
+    #     `anger`,
+    #     `fear`,
+    #     `surprise`,
+    #     `valence`,
+    #     `engagement`,
+    #     key = "emotion",
+    #     value = "value") %>%
+    #   select(emotion, value, time, key)
+    # 
+    # ggplot(emo2) +
+    #   geom_bar(aes(x=emotion, y=value, fill=emotion)) +
+    #   facet_wrap(~ key, ncol=2) +
+    #   labs(title ="Mean Affectiva Emotions Across Participants", subtitle="June 26, 2017 to July 28, 2017") +
+    #   theme_bw()
+
   })
   
 }
